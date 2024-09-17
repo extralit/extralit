3fb8f4850ae741083a003ae9911bfda20634379f
Nhat (Jonny) Tran <4750391+JonnyTran@users.noreply.github.com>
2024-09-17 21:14:26 +0000

    ci: fix test_list_schemas

--- argilla/tests/unit/cli/schemas/test_list.py
+++ argilla/tests/unit/cli/schemas/test_list.py
@@ -6,22 +6,24 @@ from rich.table import Table
 from extralit.extraction.models.schema import SchemaStructure
 from argilla.client.workspaces import Workspace
 
+from tests.factories import WorkspaceFactory
+
 if TYPE_CHECKING:
     from click.testing import CliRunner
     from pytest_mock import MockerFixture
     from typer import Typer
 
 
+@pytest.mark.asyncio
 @pytest.mark.usefixtures("login_mock")
 class TestSuiteSchemaCommands:
-    def test_list_schemas(
+    async def test_list_schemas(
         self, cli_runner: "CliRunner", cli: "Typer", mocker: "MockerFixture"
     ) -> None:
-        workspace_from_name_mock = mocker.patch("argilla.client.workspaces.Workspace.from_name")
+        workspace = await WorkspaceFactory(name="unit-test")
         list_files_mock = mocker.patch("argilla.client.workspaces.Workspace.list_files")
 
-        result = cli_runner.invoke(cli, "schemas --workspace unit-test list --versions")
+        result = cli_runner.invoke(cli, f"schemas --workspace {workspace.name} list --versions")
 
         assert result.exit_code == 0
-        # list_files_mock.assert_called_once_with(ANY, include_version=True)
-        workspace_from_name_mock.assert_called_once_with("unit-test")
+        list_files_mock.assert_called_once_with(ANY, include_version=True)
