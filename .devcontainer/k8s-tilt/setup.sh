5328ef32f16e9ddc92ac9709d02b961c8afd48de
Jonny Tran <146238514+JTran-IDM@users.noreply.github.com>
2024-11-05 20:06:26 +0000

    fix: envvar

--- .devcontainer/k8s-tilt/setup.sh
+++ .devcontainer/k8s-tilt/setup.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
+mkdir -p /tmp/k3d-volumes/elasticsearch /tmp/k3d-volumes/postgresql /tmp/k3d-volumes/minio /tmp/k3d-volumes/weaviate
+
 # Create k3d cluster for local development with ctlptl and Tilt
 if ! ctlptl get registry | grep -q "ctlptl-registry"; then
     ctlptl create registry ctlptl-registry --port=5005
     ctlptl apply -f examples/deployments/k8s/k3d/k3d-config.yaml
 else
-    echo "Registry ctlptl-registry already exists. Skipping creation."
+    echo 'Registry ctlptl-registry already exists. Skipping creation.'
 fi
 
-# Set up cron job to prune Docker builder cache every 30minutes to clean up disk space
-(crontab -l 2>/dev/null; echo "*/15 * * * * /workspace/prune_docker.sh") | crontab -
+# Set up cron job to prune Docker builder cache every 15 minutes to clean up disk space
+(crontab -l 2>/dev/null; echo '*/15 * * * * /workspace/prune_docker.sh') | crontab -
 
 # Perform the pip editable install
 if ! pip list | grep -q "extralit"; then
-    echo "Installing required packages and editable installs..."
-    uv pip install -e /workspaces/extralit/argilla-server/ && uv pip install -e /workspaces/extralit/argilla/ &
+    echo 'Installing required packages and editable installs...'
+    uv pip install -e /workspaces/extralit/argilla-server/
+    uv pip install -e /workspaces/extralit/argilla/
 else
-    echo "Package 'extralit' is already installed. Skipping installation."
+    echo 'Package 'extralit' is already installed. Skipping installation.'
 fi
 
 # Check if the upstream remote already exists
 git config --global --add safe.directory /workspaces/extralit
 if ! git remote get-url upstream &>/dev/null; then
-    echo "Adding upstream remote..."
+    echo 'Adding upstream remote...'
     git remote add upstream https://github.com/argilla-io/argilla
     git fetch upstream --no-tags
     git update-index --assume-unchanged examples/deployments/k8s/extralit-configs.yaml
     git update-index --assume-unchanged examples/deployments/k8s/extralit-deployment.yaml
 else
-    echo "Upstream remote already exists. Skipping addition."
+    echo 'Upstream remote already exists. Skipping addition.'
 fi
 
 
-echo "Setup script completed. Run tilt with `tilt up`"
+echo 'Setup script completed'
