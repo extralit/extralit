3a7187f285b1a5ce703a423b36fae6f487295eb9
Dianne Ting <109550569+LiChiaTing@users.noreply.github.com>
2024-11-13 02:30:21 +0000

    fix: `update_response` duration update

--- argilla-server/src/argilla_server/validators/responses.py
+++ argilla-server/src/argilla_server/validators/responses.py
@@ -57,7 +57,7 @@ class ResponseValidator:
         question_names = [question.name for question in record.dataset.questions]
 
         for value_question_name in response.values or []:
-            if value_question_name not in question_names:
+            if value_question_name not in question_names and value_question_name != 'duration':
                 raise UnprocessableEntityError(
                     f"found response value for non configured question with name={value_question_name!r}"
                 )
